406cbbab677c2578107ee554197b68ab41e575a0
Raphael <phael92br@gmail.com>
2022-05-03 19:35:02 -0300

    removendo nav e footer das views

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-
 // Imports
-const express = require('express');
-const layouts = require('express-ejs-layouts')
+const express = require("express");
+const layouts = require("express-ejs-layouts");
 
 // Importar os roteadores
-const indexRouter = require('./routes/indexRouter');
-const cadastroRouter = require('./routes/cadastroRouter');
+const indexRouter = require("./routes/indexRouter");
+const cadastroRouter = require("./routes/cadastroRouter");
 
 // Criar um servidor/aplicação com o express
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static('public'));
-app.use(layouts)
+app.use(express.static("public"));
+app.use(layouts);
 
 // Configurar o ejs como um template engine
-app.set('layout', './layouts/nav-footer')
-app.set('view engine', 'ejs')
+app.set("layout", "./layouts/nav-footer");
+app.set("view engine", "ejs");
 
 // Usando os roteadores
-app.use('/', indexRouter);
+app.use("/", indexRouter);
+app.use("/", cadastroRouter);
 
 // Levantar/rodar/executar a nossa aplicação
-app.listen(3000, ()=>{console.log("Aplicação escutando a porta 3000")} );
+app.listen(3000, () => {
+  console.log("Aplicação escutando a porta 3000");
+});
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,11 +1,14 @@
 module.exports = {
-    showHomePage: (req, res) => {
-        res.render('index')
-    },
-    showCadastro: (req, res) => {
-        res.render('cadastro')
-    },
-    showProdutos: (req, res) => {
-        res.render('produto')
-    }
-}
+  showHomePage: (req, res) => {
+    res.render("index");
+  },
+  showCadastro: (req, res) => {
+    res.render("cadastro");
+  },
+  showProdutos: (req, res) => {
+    res.render("produto");
+  },
+  showLogin: (req, res) => {
+    res.render("login");
+  },
+};
