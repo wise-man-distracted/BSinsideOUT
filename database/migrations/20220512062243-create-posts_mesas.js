8830ef8057a7ed66cf9d40b34678e92a850a1908
Raphael <phael92br@gmail.com>
2022-05-12 03:41:58 -0300

    criando migrations de posts_mesas e usuarios_in_mesas

---
+++ database/migrations/20220512062243-create-posts_mesas.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('posts_mesas',
+    {
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+      },      
+      pinned: {
+        type: Sequelize.DataTypes.TINYINT(1),
+        allowNull: true
+      },
+      status: {
+        type: Sequelize.DataTypes.TINYINT(1),
+        allowNull: true
+      },
+      post: {
+        type: Sequelize.DataTypes.TEXT,
+        allowNull: true
+      },      
+      mesas_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        // AQUI SERÁ UMA FUTURA FK
+        // references: {
+        //   model:{
+        //     tableName: "mesas"
+        //   },
+        //   key:"id"
+        // }
+      },
+      usuarios_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        // AQUI SERÁ UMA FUTURA FK
+        // references: {
+        //   model:{
+        //     tableName: "usuarios"
+        //   },
+        //   key:"id"
+        // }
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('posts_mesas');
+  }
+};
---
+++ database/migrations/20220512063653-create-usuarios_in_mesas.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('usuarios_in_mesas',
+    {
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+      },
+      status: {
+        type: Sequelize.DataTypes.TINYINT(1),
+        allowNull: true
+      },  
+      usuarios_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        // AQUI SERÁ UMA FUTURA FK
+        // references: {
+        //   model:{
+        //     tableName: "usuarios"
+        //   },
+        //   key:"id"
+        // }
+      },
+      mesas_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        // AQUI SERÁ UMA FUTURA FK
+        // references: {
+        //   model:{
+        //     tableName: "mesas"
+        //   },
+        //   key:"id"
+        // }
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('usuarios_in_mesas');
+  }
+};
