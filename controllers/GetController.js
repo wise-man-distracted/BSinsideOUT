50d4e5e6567e3907c6b8750d3f6c27d84b320f8f
wise-man-distracted <magewelle@live.com>
2022-07-15 15:23:19 -0300

    front: artigos dinâmicos

--- controllers/GetController.js
+++ controllers/GetController.js
@@ -1,4 +1,4 @@
-const { Produto, Artigo, Categoria_Artigo } = require('../models');
+const { Produto, Artigo } = require('../models');
 
 module.exports = {
   showHomePage: (req, res) => {
@@ -13,10 +13,10 @@ module.exports = {
     let produto; let descricoes; let preco
 
     try {
-      produto = await Produto.findOne({where:{id}})
-      produto = produto.dataValues
+      produto = await Produto.findOne({where:{id}, raw: true, nest: true})
       descricoes = produto.descricao.split('\n')
-      preco = String(produto.preco).slice(0, String(produto.preco).length-2)
+      //preco = String(produto.preco).slice(0, String(produto.preco).length-2)
+      preco = produto.preco
       res.render("produto", {produto, descricoes, preco})
     } catch (error) {
       return res.render('error', {error: "O servidor pode estar ocupado numa sidequest. Tente novamente mais tarde", status: 503})
@@ -35,14 +35,12 @@ module.exports = {
     let id = req.params.id
     let artigos; let categorias
     try {
-      artigos = await Artigo.findAll({include: 'categoria', raw: true, nest: true})
-      categorias = await Categoria_Artigo.findAll()
-      //categorias = categorias.dataValues
-      console.log(artigos)
+      artigos = await Artigo.findAll({include: ['categoria', 'autor'], raw: true, nest: true})
+      console.log(artigos[0].autor)
     } catch(error) {
       return res.render('error', {error: "O servidor pode estar ocupado numa sidequest. Tente novamente mais tarde", status: 503})
     }
-    res.render("inside", {artigos, categorias});
+    res.render("inside", {artigos});
   },
   showArticle: (req, res) => {
     let id = req.params.id
