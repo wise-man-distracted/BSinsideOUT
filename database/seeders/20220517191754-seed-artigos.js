7be607a1a11fdbf8eb2c9c0368706e528714dcd8
Raphael <phael92br@gmail.com>
2022-07-21 02:04:06 -0300

    adicionando alguns dados ao blog

--- database/seeders/20220517191754-seed-artigos.js
+++ database/seeders/20220517191754-seed-artigos.js
@@ -6,93 +6,41 @@ module.exports = {
      await queryInterface.bulkInsert('artigos', [
     {
       id: 1,
-      titulo: "Cartas de Magic que quebram os torneios de constructed.",
-      conteudo: `Quis deserunt consectetur velit id aliquip nisi fugiat culpa nisi dolore commodo ullamco laboris quis. Anim aute velit mollit aliquip quis incididunt exercitation mollit aliquip cillum magna aliqua dolor adipisicing. Nostrud ullamco ullamco ex incididunt labore occaecat. Laborum labore pariatur sint enim ut. Ea do nulla culpa laborum nulla ipsum deserunt in dolore qui. Veniam fugiat ea minim nulla laborum.
-
-Nostrud labore veniam incididunt exercitation veniam dolore laborum dolor dolore. Lorem incididunt ut culpa deserunt id adipisicing elit magna irure eiusmod ullamco laboris elit eiusmod. Voluptate laborum duis elit excepteur laborum deserunt qui consequat nulla Lorem exercitation Lorem ullamco. Amet culpa veniam adipisicing ex non. Anim ad irure ex eiusmod dolor tempor mollit nulla laboris.
-
-Aliquip do adipisicing laborum sit laborum id ad nisi nulla sit ex ad mollit eiusmod. Adipisicing reprehenderit ut ea et reprehenderit Lorem ex ea aliquip eu. Commodo enim consequat nostrud adipisicing do dolor. Voluptate est voluptate exercitation occaecat. Nulla reprehenderit cupidatat est qui fugiat magna reprehenderit id et veniam est ad aliquip.
-
-Minim est excepteur id voluptate ullamco nostrud dolore commodo irure qui cupidatat pariatur. Dolor aliquip dolore ipsum ex nulla ullamco anim dolor veniam. Pariatur officia sunt ad cillum anim eiusmod esse anim magna aliquip duis minim ullamco. Amet aliqua ex consequat nulla elit irure reprehenderit non sit eiusmod qui irure.
-
-Ut duis dolore eu labore ad minim nostrud enim ullamco minim. Laborum nostrud est ipsum minim non Lorem labore cupidatat et cillum consequat labore. Fugiat deserunt esse esse ullamco minim. Nostrud ad minim consequat dolore velit ipsum minim ad sit aliqua deserunt.
-
-Nulla consectetur ullamco Lorem esse tempor sunt cillum consectetur minim ad ullamco sit. Enim aliquip anim in duis occaecat minim culpa do consequat adipisicing sunt cupidatat tempor pariatur. Enim adipisicing nostrud Lorem sit sunt incididunt dolore excepteur ipsum sit irure Lorem consequat. Labore commodo ut est adipisicing dolore minim est. Anim pariatur irure velit officia laboris in sunt pariatur. Aute sint consequat consequat dolore est esse eiusmod duis dolore nulla fugiat.
-
-Mollit commodo velit eiusmod quis eu reprehenderit sint adipisicing magna. Laborum id laborum irure non occaecat adipisicing nulla non aliquip pariatur Lorem qui. Et aute anim elit anim id adipisicing nisi culpa nostrud anim do dolore est. Consectetur consectetur eiusmod velit ullamco in adipisicing esse dolor est amet. Sunt enim consectetur nulla adipisicing dolore exercitation ad ipsum velit.
-
-Est ipsum eiusmod fugiat laboris nulla eiusmod duis. Deserunt velit consectetur nostrud magna officia. Enim sunt ea id sunt eiusmod et ea minim commodo irure voluptate reprehenderit. Anim exercitation incididunt exercitation minim tempor esse elit. Anim proident commodo Lorem labore incididunt deserunt eiusmod aute qui quis Lorem do tempor duis.
-
-Anim cillum sunt duis occaecat. Ipsum adipisicing in non nisi ipsum fugiat do eiusmod voluptate in in consectetur non. Magna quis consectetur nostrud pariatur quis id ad aute commodo elit cupidatat id deserunt id. Ipsum sit consectetur nisi do aliqua aliqua occaecat ea amet. Minim consequat dolore magna aliquip magna voluptate esse incididunt irure. Est Lorem anim mollit dolor exercitation elit.
-
-Ad aliqua nisi cupidatat cillum quis enim aute sit sint. Eiusmod veniam sit culpa ex duis dolor culpa. Culpa magna magna mollit consectetur quis pariatur sint eiusmod nulla laborum aute. Cillum do id labore culpa cupidatat aliquip sit cillum velit aliqua cillum.`,
+      titulo: "D&D: Unearthed Arcana: Opções Dracônicas",
+      conteudo: `A primeira é um trio de opções de raça dracônica apresentadas como uma alternativa à raça draconato do Livro do Jogador, bem como uma nova visão da raça kobold. Em seguida, vem um punhado de opções de talentos que refletem uma conexão com o poder dracônico. Finalmente, uma seleção de feitiços – muitos deles com nomes de dragões famosos ou infames – oferece uma variedade de abordagens para manifestar a magia dracônica. A versão final desse material saiu nos livros Fizban Treasury of Dragons e Mordenkainen Presents: Monsters of the Multiverse.`,
       data: new Date(),
-      descricao_curta: "Eu fugiat proident voluptate laboris occaecat tempor ut commodo est ad cupidatat elit dolor.",
-      img_1: "https://images.unsplash.com/photo-1611104866937-e7e05b8571b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
-      img_2: "https://images.unsplash.com/photo-1605420801008-456056f2c700?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-      img_3: "https://images.unsplash.com/photo-1530451918823-b2c35b34f7e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      descricao_curta: "Aproveite o poder dos dragões nesta edição da Unearthed Arcana",
+      img_1: "https://www.rederpg.com.br/wp/wp-content/uploads/2016/07/Article_Feats-864x467.jpg",
+      img_2: "",
+      img_3: "",
       usuarios_id: 2,
       categorias_id: 3,
       createdAt: new Date()
     },
     {
       id: 2,
-      titulo: "A influência da idade no interesse por jogos narrativos ou estratégicos.",
-      conteudo: `Laborum officia est proident ullamco laborum ut occaecat ad. Duis tempor sint non excepteur et labore laborum ea duis laborum. Aute laborum incididunt esse consectetur ut consequat nostrud fugiat nisi ad anim. Est ut non ad ut sunt ex non laborum in do qui sunt. Laboris occaecat aliqua mollit aliquip. Eu reprehenderit est veniam deserunt.
-
-Deserunt est in id ex ea duis qui veniam velit labore est occaecat enim. Occaecat aute et ea do. Amet officia eu ad do cillum. Occaecat incididunt magna commodo id magna do do magna exercitation consectetur velit cillum. Irure minim officia in do.
-
-Mollit id non ut consequat id et enim laboris. Commodo dolore Lorem deserunt magna deserunt dolore fugiat aliqua cupidatat exercitation culpa voluptate ex. Ex est excepteur reprehenderit incididunt sint dolor mollit adipisicing.
-
-Enim incididunt occaecat fugiat anim. Qui sint mollit laborum et cupidatat et velit non labore nulla ut cillum. Lorem excepteur elit est dolor est ipsum nisi fugiat reprehenderit Lorem elit. Quis ex mollit dolor fugiat aliquip.
-
-Officia aute laborum voluptate sint culpa consectetur eu magna. Adipisicing minim cillum voluptate adipisicing proident culpa Lorem quis. Veniam exercitation nisi enim dolore. Dolor sint ipsum incididunt cillum duis magna incididunt aliqua laborum aliqua elit Lorem ullamco qui. Occaecat veniam aliquip laboris non nisi cillum proident labore velit est quis.
-
-Nostrud ipsum nulla voluptate eiusmod ut. Nulla elit irure non non ipsum. Ex adipisicing mollit anim duis. Excepteur eu ea anim do non eu occaecat consequat ad. Sunt in laborum elit ad id ipsum consequat reprehenderit nisi Lorem. Reprehenderit amet incididunt Lorem excepteur proident sint ut nisi dolore est ipsum ex. Ut fugiat ut deserunt amet.
-
-Esse ex magna minim exercitation sunt anim nisi eu. Veniam excepteur cillum esse laboris aliquip veniam elit velit proident irure ex non duis. Ullamco eu minim dolore laboris ea mollit culpa officia quis duis adipisicing ut quis. Commodo quis ad non quis duis sunt elit cupidatat aute dolor culpa adipisicing dolor.
-
-Laborum do elit minim excepteur exercitation proident mollit ea incididunt. Culpa commodo ea tempor commodo qui sit esse cillum dolor excepteur proident. Esse culpa tempor exercitation veniam pariatur. Quis cupidatat incididunt veniam fugiat officia cillum consequat non mollit minim nulla.
-
-Amet anim pariatur amet laborum quis fugiat excepteur culpa est Lorem. Mollit Lorem mollit nostrud occaecat Lorem eu pariatur dolore deserunt. Occaecat cillum veniam deserunt incididunt ipsum. In voluptate laboris do fugiat. Irure ad aliqua occaecat in est ex est tempor amet esse. Fugiat qui elit laboris enim laborum dolor id anim adipisicing. Adipisicing culpa aliqua exercitation incididunt incididunt mollit esse ex minim veniam exercitation nisi laborum.
-
-Incididunt aliquip proident ad et ea minim in mollit ut aliqua dolor ullamco labore. Sunt consectetur cillum cupidatat elit consectetur sunt veniam Lorem esse aliquip consectetur ad. Est esse ea aliquip elit ex officia adipisicing voluptate commodo eiusmod adipisicing velit consectetur. Mollit velit ut proident in irure qui esse ea eu. Do irure duis officia mollit dolor consequat sit sint officia cillum dolore exercitation. Eiusmod occaecat enim excepteur in amet ipsum voluptate eiusmod.`,
+      titulo: "10 Harsh Realities Of Being A Magic: The Gathering Fan",
+      conteudo: `Magic: The Gathering has been going strong since its release in August 1993 as the world's first trading card game, later joined by its competitors such as Yu-Gi-Oh!, the Pokémon TCG, and more. MTG certainly had some growing pains, such as getting rid of the gambling-style ante rules and figuring out the rules for legendary creatures, but the game is bigger and better than ever now.`,
       data: new Date(),
-      descricao_curta: "Eu fugiat proident voluptate laboris occaecat tempor ut commodo est ad cupidatat elit dolor.",
-      img_1: "https://images.unsplash.com/photo-1595345639192-5ae79fc1a715?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-      img_2: "https://images.unsplash.com/photo-1513159446162-54eb8bdaa79b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      img_3: "https://images.unsplash.com/photo-1591334770544-a83728c5768b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8b2xkJTIwbWFuJTIwcGxheWluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      descricao_curta: "Playing Magic: The Gathering is wonderful, but this game can also be a tough hobby to pursue at times.",
+      img_1: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/05/Featured-image-for-an-article-about-the-harsh-realities-of-being-a-Magic-The-Gathering-fan.jpg?q=50&fit=contain&w=750&h=&dpr=1.5",
+      img_2: "",
+      img_3: "",
       usuarios_id: 1,
       categorias_id: 2,
       createdAt: new Date()
     },
     {
       id: 3,
-      titulo: "Melhores opções gastronômicas para encontros rpgísticos.",
-      conteudo: `Id eu qui mollit voluptate deserunt nisi id ut voluptate esse aliquip cupidatat dolor ea. Lorem nulla aliqua adipisicing nostrud cupidatat. Eu officia nostrud magna nisi sunt aliqua deserunt sunt magna sint consequat officia cupidatat aliquip. Culpa officia ut est ad nisi. Ad qui deserunt enim cupidatat excepteur sint excepteur laborum mollit.
+      titulo: "Claymore | Quando o animê supera um já excelente mangá",
+      conteudo: `Claymore é a história de Clare, membro de uma organização de mulheres que lutam contra os Yoma, espécie de demônio que se alimenta de carne humana, em troca de dinheiro. Para serem capazes de derrotá-los, porém, carne e sangue Yoma foram inseridos em seus corpos, fazendo com que se tornassem metade humanas, metade Yoma. Essas guerreiras são chamadas de “Claymores” devido ao tipo de espadas que usam, que leva esse nome, ou de “Bruxas dos Olhos Prateados”, culpa da coloração que seus olhos recebem após se tornarem membros da organização.
 
-Consequat est do non excepteur esse cupidatat. Occaecat laboris culpa anim enim in Lorem enim laboris. Adipisicing veniam mollit ullamco nulla minim labore. Ad esse pariatur cupidatat exercitation Lorem laboris nulla reprehenderit et aliquip elit laboris.
-
-Reprehenderit minim occaecat nostrud id ut duis deserunt. Pariatur et sit do irure irure deserunt et consectetur. Occaecat sunt nisi duis qui ad magna Lorem non aute. Eu exercitation aliquip eu ex deserunt ad irure do aute.
-
-Ut elit commodo laborum consequat proident sunt velit commodo ad. Tempor sunt culpa aliqua incididunt dolor in ipsum id. Ipsum minim in nostrud dolore exercitation. Ea pariatur commodo consequat anim sit.
-
-Occaecat commodo dolor exercitation ullamco aliqua. Nisi non et et excepteur deserunt aliqua ea adipisicing nisi magna excepteur in mollit aliquip. Pariatur duis dolore sunt reprehenderit cillum cillum fugiat minim tempor et culpa aliquip magna. Cupidatat aliquip reprehenderit elit fugiat reprehenderit nostrud. Sint magna deserunt sit incididunt ex amet. Qui elit aliquip anim duis ullamco tempor labore amet irure id incididunt sint eiusmod. Do exercitation consequat adipisicing esse enim minim dolore dolore.
-
-Quis aute irure commodo sunt nostrud mollit sint amet. Exercitation deserunt dolor mollit mollit aliqua exercitation occaecat enim duis do fugiat. Est minim proident consectetur magna commodo aliqua anim officia ullamco consectetur qui velit. Sint velit occaecat cupidatat consequat irure Lorem in nostrud anim laboris duis velit eiusmod. Aliqua commodo incididunt ipsum minim nostrud ullamco fugiat ut sit incididunt irure labore qui et. Sit nulla mollit occaecat ipsum qui do aute. Reprehenderit mollit proident quis velit pariatur occaecat aute labore deserunt et officia ullamco sit.
-
-Officia voluptate anim eiusmod ex Lorem ipsum. Aliquip elit ullamco ea sit id officia consequat. Elit laboris laboris aliqua qui ad et ut cupidatat.
-
-Commodo commodo culpa nostrud est cillum elit minim enim nostrud aute labore culpa. Lorem Lorem ullamco non dolore enim aute proident anim dolor. Cupidatat tempor est ipsum deserunt Lorem tempor reprehenderit dolore id qui irure.
-
-Mollit commodo deserunt labore ad dolore eiusmod eiusmod eiusmod sit sit Lorem eiusmod sunt labore. Eiusmod voluptate dolore aute do magna. Aliqua irure adipisicing sunt cillum elit enim id aliquip incididunt. Irure culpa nisi pariatur pariatur. Consequat incididunt deserunt sint do ad est quis tempor aute nostrud ad. Amet voluptate amet id irure nisi enim excepteur dolore Lorem. Quis adipisicing dolor sunt id Lorem est incididunt duis velit id ipsum duis.
-
-Commodo officia labore consequat pariatur irure adipisicing aute magna ex do anim. Ut laborum magna esse excepteur qui velit aute. Ullamco magna proident dolore reprehenderit excepteur laborum dolor amet nostrud aliqua dolor ex dolore nisi.`,
+`,
       data: new Date(),
       descricao_curta: "Eu fugiat proident voluptate laboris occaecat tempor ut commodo est ad cupidatat elit dolor.",
-      img_1: "https://images.unsplash.com/photo-1517499414974-3b42addf2d86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      img_2: "https://images.unsplash.com/photo-1497395217579-0d6deaff0906?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      img_3: "https://images.unsplash.com/photo-1528699633788-424224dc89b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=433&q=80",
+      img_1: "https://www.portallos.com.br/wp-content/uploads/2009/11/claymore.jpg",
+      img_2: "",
+      img_3: "",
       usuarios_id: 3,
       categorias_id: 1,
       createdAt: new Date()
