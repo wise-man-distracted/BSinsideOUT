c55c7b043873a5c03557e71a612a664cd3258b44
wise-man-distracted <magewelle@live.com>
2022-07-22 15:37:41 -0300

    delete method final

--- controllers/UpdateController.js
+++ controllers/UpdateController.js
@@ -1,5 +1,15 @@
+const { Produto } = require('../models');
+
 module.exports = {
-    deletarProduto: (req, res) => {
-        res.send('rota delete funfanfo')
-    }
+    deletarProduto: async (req, res) => {
+        const { id } = req.body
+        let produto
+        try {
+            produto = await Produto.findOne({where:{id}, raw: true, nest: true})
+            res.send(produto)
+        } catch (error) {
+            res.send(error)
+        }
+    },
+    
 }
