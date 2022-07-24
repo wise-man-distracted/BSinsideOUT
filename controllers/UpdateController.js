73ae9fa60275aae42e4d59eefca850ed407ac1bc
wise-man-distracted <magewelle@live.com>
2022-07-24 06:09:04 -0300

    update & delete funcional

--- controllers/UpdateController.js
+++ controllers/UpdateController.js
@@ -5,35 +5,31 @@ module.exports = {
         const { id } = req.body
         let produto
         try {
-            produto = await Produto.findOne({where:{id}, raw: true, nest: true})
-            res.send(produto)
+            produto = await Produto.destroy({where:{id}})
+            res.render('success', {msg: "Produto deletado com sucesso.", header: "SUCESSO", usuario: req.usuario})
         } catch (error) {
             res.send(error)
         }
     },
     atualizarProduto: async (req, res) => {
-        const { id, nome, distribuidor, preco, descricao, descricao_curta, categorias_id } = req.body
+        const { id, nome, distribuidor, descricao, descricao_curta, categorias_id } = req.body
         let produto
         try {
-            produto = await Produto.findOne({where:{id}, raw: true, nest: true})
+            produto = await Produto.findOne({where:{id}})
 
             await produto.update({
                 nome : nome,
                 distribuidor : distribuidor,
-                preco : preco,
                 descricao : descricao,
                 descricao_curta : descricao_curta,
                 categorias_id : categorias_id
             })
 
             // await produto.save({fields: ['nome', 'distribuidor', 'preco', 'descricao', 'descricao_curta', 'categorias_id']})
-            res.send("okay")
+            res.render('success', {header: "SUCESSO", msg: "Produto atualizado com sucesso", usuario: req.usuario})
         } catch (error) {
             res.send(error)
         }
-
-
-        res.send([id, nome, distribuidor, preco, descricao, descricao_curta, categorias_id])
     }
 
 }
